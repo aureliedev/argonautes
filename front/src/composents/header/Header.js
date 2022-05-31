3c4b64f4e60d2959e18da10787fdcf5c993398bd
Aurelie Menin <atypik77@hotmail.fr>
2022-05-31 16:53:26 +0200

    add header + css ok

---
+++ front/src/composents/header/Header.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./header.css";
+
+function Header() {
+  return (
+    <header className="header">
+      <h1>
+        <img
+          src="https://www.wildcodeschool.com/assets/logo_main-e4f3f744c8e717f1b7df3858dce55a86c63d4766d5d9a7f454250145f097c2fe.png"
+          alt="Wild Code School logo"
+        />
+      </h1>
+    </header>
+  );
+}
+
+export default Header;
