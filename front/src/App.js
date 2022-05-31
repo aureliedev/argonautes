e00016e7b7abfa270a9d7898b87bdb2cd8989bcd
Aurelie Menin <atypik77@hotmail.fr>
2022-05-31 17:00:45 +0200

    add footer.js fotter.css ok

--- front/src/App.js
+++ front/src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "./composents/header/Header";
+import Footer from "./composents/footer/Footer";
 
 function App() {
   return (
     <div>
       <Header />
+      <Footer />
     </div>
   );
 }
