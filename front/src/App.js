29d4e82e73b23908882b91e02687b736678c0200
Aurelie Menin <atypik77@hotmail.fr>
2022-05-31 17:23:41 +0200

    ajout axios structure mail.js et css

--- front/src/App.js
+++ front/src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Header from "./composents/header/Header";
+import Main from "./composents/main/Main";
 import Footer from "./composents/footer/Footer";
 
 function App() {
   return (
     <div>
       <Header />
+      <Main />
       <Footer />
     </div>
   );
