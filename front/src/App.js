b4d1d4b27c0a0eed853afc311a49aa0c397d2aef
Aurelie Menin <atypik77@hotmail.fr>
2022-06-02 18:55:40 +0200

    mise en forme body

--- front/src/App.js
+++ front/src/App.js
@@ -3,10 +3,11 @@ import Header from "./composents/header/Header";
 import Form from "./composents/Main/form/Form";
 import Members from "./composents/Main/members/Members";
 import Footer from "./composents/footer/Footer";
+import "./app.css";
 
 function App() {
   return (
-    <div>
+    <div className="body">
       <Header />
       <Form />
       <Members />
